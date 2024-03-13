3e7d1a7a14dde8ca446ff22101aed869ce370742
katyu666a <kruekaterinarusakevich@gmail.com>
2024-03-13 13:43:43 +0300

    Create ui.components, Note.kt

---
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -0,0 +1,14 @@
+package com.topic2.android.notes.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Note() {
+
+}
+@Preview
+@Composable
+private fun NotePreview() { Note()
+
+}
