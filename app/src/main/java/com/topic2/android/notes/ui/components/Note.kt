388d40d715e818c77d6698e39f2d81cb24493ddb
katyu666a <kruekaterinarusakevich@gmail.com>
2024-03-13 13:57:49 +0300

    Create Note 2

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -2,6 +2,9 @@ package com.topic2.android.notes.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Checkbox
@@ -11,20 +14,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
+import java.nio.file.WatchEvent
 
 @Composable
 fun Note() {
-Box(
-    modifier = Modifier
-        .size(40.dp)
-        .background(rwGreen)
-)
-    Text(text = "Заголовок", maxLines = 1)
-    Text(text = "Содержимое", maxLines = 1)
-    Checkbox(
-        checked = false, onCheckedChange = { },
-        modifier = Modifier.padding(start = 8.dp)
-    )
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(rwGreen)
+        )
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Заголовок", maxLines = 1)
+            Text(text = "Содержимое", maxLines = 1)
+        }
+        Checkbox(
+            checked = false,
+            onCheckedChange = { },
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
 }
 @Preview
 @Composable
