29e1a5291f4121ce1c7162a831f73c7096c70d88
katyu666a <kruekaterinarusakevich@gmail.com>
2024-03-25 22:54:35 +0300

    Addition Note()

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -19,10 +19,11 @@ import java.nio.file.WatchEvent
 @Composable
 fun Note() {
     Row(modifier = Modifier.fillMaxWidth()) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(rwGreen)
+        NoteColor(
+            color = rwGreen,
+            size = 40.dp,
+            padding = 4.dp,
+            border = 1.dp
         )
         Column(modifier = Modifier.weight(1f)) {
             Text(text = "Заголовок", maxLines = 1)
