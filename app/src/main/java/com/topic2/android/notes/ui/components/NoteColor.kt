88148c57038f6df2ac567e964d6b0597faefe5e4
katyu666a <kruekaterinarusakevich@gmail.com>
2024-03-25 22:40:05 +0300

    Create in NoteColor the border.

--- app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
@@ -1,5 +1,8 @@
 package com.topic2.android.notes.ui.components
+
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -7,8 +10,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 @Composable
 fun NoteColor() {
     Box(
@@ -17,6 +22,13 @@ fun NoteColor() {
             .clip(CircleShape)
             .background(Color.Red)
 
+            .border(
+                BorderStroke(
+                    2.dp,
+                    SolidColor(Color.Black)
+                ),
+                CircleShape
+            )
     )
 }
 
