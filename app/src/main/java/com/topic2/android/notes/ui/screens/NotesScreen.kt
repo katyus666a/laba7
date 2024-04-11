892474e10e080c64c1fd000dbdb2c449c885bfed
katyu666a <kruekaterinarusakevich@gmail.com>
2024-04-11 22:21:48 +0300

    Create NotesScreen.kt

---
+++ app/src/main/java/com/topic2/android/notes/ui/screens/NotesScreen.kt
@@ -0,0 +1,12 @@
+package com.topic2.android.notes.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import com.topic2.android.notes.viewmodel.MainViewModel
+
+@Composable
+fun NotesScreen(
+    viewModel: MainViewModel
+) {
+
+}
